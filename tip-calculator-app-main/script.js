0c2205fd1735ab03dcf80068e3cbf1d1168ff0e5
ssj24 <sujee0824@gmail.com>
2022-04-01 20:13:09 +0900

    solution

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,36 +1,44 @@
-const radioBtns = document.querySelector('.radioBtns');
-const tips = ['5', '10', '15', '25', '50', 'Custom'];
 const numInputs = document.querySelectorAll('.numInput');
 const tip = document.querySelector('.tip');
 const total = document.querySelector('.total');
-const radio = document.getElementsByName('tips');
-radio.value = 5;
-let radioInput = '';
+const radio = document.querySelectorAll('input[name="tips"]');
+const tipCustom = document.querySelector('.perCustom');
+const resetBtn = document.querySelector('.resetBtn');
+let tipValue = 5;
 
-for (let i = 0; i < tips.length; i++) {
-    if (i == tips.length - 1) {
-        radioInput = `
-        <input type="number" name="customTips" id="customTips" class="radioLabel per${i+1}" placeholder = "Custom">
-        `
-        radioBtns.insertAdjacentHTML('beforeend', radioInput);
-        break;
+function tipCalculate() {
+    let bill = numInputs[0].value;
+    let people = numInputs[1].value;
+    if (!(tipValue && people && bill) || people == 0) {
+        tip.innerHTML = 0.00;
+        total.innerHTML = 0.00;
+        return
     }
-    radioInput = `
-    <input type="radio" name="tips" id="per${i+1}" value=${tips[i]}>
-    <label for="per${i+1}" class="radioLabel per${i+1}">
-        <div class="radioBtn">
-        <p>${tips[i]}%</p>
-        </div>
-    </label>
-    `
-    radioBtns.insertAdjacentHTML('beforeend', radioInput);
+    tip.innerHTML = (Math.floor((bill * (tipValue/100)) / people *100)/100).toFixed(2);
+    total.innerHTML = (Math.round((bill * (1 + tipValue/100)) / people *100)/100).toFixed(2);
 }
-function tipCalculate(bill = numInputs[0].value, tipPer = radio.value, people = numInputs[1].value) {
-    console.log(tipPer);
-    tip.innerHTML = (bill / people).toFixed(2);
-    total.innerHTML = ((bill * (1 + tipPer/100)) / people).toFixed(2);
+function resetRadio() {
+    for (let i = 0; i < radio.length; i++) {
+        radio[i].checked = false;
+    }
 }
-
 for (let i = 0; i < numInputs.length; i++) {
-    numInputs[i].addEventListener('change', tipCalculate());
+    numInputs[i].addEventListener('input', function() {
+        tipCalculate();
+    });
 }
+for (let i = 0; i < radio.length; i++) {
+    radio[i].addEventListener('change', function() {
+        tipValue = radio[i].value;
+        tipCustom.value = '';
+        tipCalculate();
+    })
+}
+tipCustom.addEventListener('keyup', function(e) {
+    tipValue = e.target.value;
+    resetRadio();
+    tipCalculate();
+})
+resetBtn.addEventListener('click', function() {
+    document.querySelector('.container').reset();
+});
