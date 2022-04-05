9ff264c9e9768f51c466ddb6459711d1f0bc54fb
ssj24 <sujee0824@gmail.com>
2022-04-05 19:25:16 +0900

    solution

--- time-tracking-dashboard-main/script.js
+++ time-tracking-dashboard-main/script.js
@@ -1,48 +1,32 @@
+const menus = document.querySelectorAll('li');
+const currentTimes = document.querySelectorAll('.time > span');
+const pastPeriods = document.querySelectorAll('.period');
+const pastTimes = document.querySelectorAll('.periodDetail');
 
-//   Work
-//   5hrs <!-- daily -->
-//   Previous - 7hrs <!-- daily -->
-//   32hrs <!-- weekly -->
-//   Previous - 36hrs <!-- weekly -->
-//   103hrs <!-- monthly -->
-//   Previous - 128hrs <!-- monthly -->
+let jsonData = [];
+let myPeriod = 'daily';
+const assignData = () => {
+    let prevTarget = 'Yesterday';
+    if (myPeriod == 'weekly') prevTarget = 'Last Week';
+    else if (myPeriod == 'monthly') prevTarget = 'Last Month';
+    for (let i = 0; i < currentTimes.length; i++) {
+        currentTimes[i].innerHTML = jsonData[i].timeframes[myPeriod].current;
+        pastPeriods[i].innerHTML = prevTarget;
+        pastTimes[i].innerHTML = jsonData[i].timeframes[myPeriod].previous;
+    }
+}
 
-//   Play
-//   1hr <!-- daily -->
-//   Previous - 2hrs <!-- daily -->
-//   10hrs <!-- weekly -->
-//   Previous - 8hrs <!-- weekly -->
-//   23hrs <!-- monthly -->
-//   Previous - 29hrs <!-- monthly -->
-
-//   Study
-//   0hrs <!-- daily -->
-//   Previous - 1hr <!-- daily -->
-//   4hrs <!-- weekly -->
-//   Previous - 7hrs <!-- weekly -->
-//   13hrs <!-- monthly -->
-//   Previous - 19hrs <!-- monthly -->
-
-//   Exercise
-//   1hr <!-- daily -->
-//   Previous - 1hr <!-- daily -->
-//   4hrs <!-- weekly -->
-//   Previous - 5hrs <!-- weekly -->
-//   11hrs <!-- monthly -->
-//   Previous - 18hrs <!-- monthly -->
-
-//   Social
-//   1hr <!-- daily -->
-//   Previous - 3hrs <!-- daily -->
-//   5hrs <!-- weekly -->
-//   Previous - 10hrs <!-- weekly -->
-//   21hrs <!-- monthly -->
-//   Previous - 23hrs <!-- monthly -->
-
-//   Self Care
-//   0hrs <!-- daily -->
-//   Previous - 1hr <!-- daily -->
-//   2hrs <!-- weekly -->
-//   Previous - 2hrs <!-- weekly -->
-//   7hrs <!-- monthly -->
-//   Previous - 11hrs <!-- monthly -->
+const loadJson = async () => {
+    await fetch('./data.json')
+        .then(response => response.json())
+        .then(data => jsonData = data)
+        .catch(error => console.log(error));
+    assignData()
+}
+loadJson();
+menus.forEach((menu) => {
+    menu.addEventListener('click', () => {
+        myPeriod = menu.innerHTML.toLowerCase();
+        assignData()
+    });
+})
