4c7e19898fd59cfa83cd606bfcaf99df5e50c55e
ssj24 <sujee0824@gmail.com>
2022-07-12 14:05:02 +0900

    Update layout

--- todo-app-main/script.js
+++ todo-app-main/script.js
@@ -1,10 +1,12 @@
 const container = document.getElementById('container');
 const theme = document.getElementById('theme');
-
+const addContainer = document.querySelector('.addContainer');
+const lists = document.querySelectorAll('.listItem');
+const delBtns = document.querySelectorAll('.delBtn');
 let curTheme = true;
 
 const changeTheme = () => {
-  container.classList.toggle('dark');
+  document.body.classList.toggle('dark');
   curTheme = !curTheme;
   if (curTheme) {
     theme.childNodes[1].src = "./images/icon-moon.svg";
@@ -15,4 +17,17 @@ const changeTheme = () => {
   }
 };
 
+const toggleActive = function() {
+  this.firstElementChild.classList.toggle('active');
+}
+
 theme.addEventListener('click', changeTheme);
+addContainer.addEventListener('click', toggleActive);
+for (let list of lists) {
+  list.addEventListener('click', toggleActive)
+}
+for (let delBtn of delBtns) {
+  delBtn.addEventListener('click', e => {
+    e.stopPropagation();
+  })
+}
